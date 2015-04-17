70e45a944a5ea9dea1a247468f19d9fc336c9f1d
FlashCat <FlashCat@users.noreply.github.com>
2015-04-17 15:43:21 +0000

    rebuild pages at 9b3ef6c

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['linear_map'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;K: <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a>, V: <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='linear_map/struct.LinearMap.html' title='linear_map::LinearMap'>LinearMap</a>&lt;K, V&gt;",];
+implementors['linear_map'] = ["impl&lt;K: <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a>, V: <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='linear_map/struct.LinearMap.html' title='linear_map::LinearMap'>LinearMap</a>&lt;K, V&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
